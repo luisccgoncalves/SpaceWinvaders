a20ff5fb7391bd5ae4677c62525b6723998f9d0e
luisccgoncalves <luisccgoncalves@gmail.com>
2018-04-26 20:40:26 +0100

    moved boot_gateway function from server to a new launcher

---
+++ Launcher/main.c
@@ -0,0 +1,27 @@
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <io.h>
+
+int boot(TCHAR * pProc) {
+
+	TCHAR	procname[20];
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
+
+	_tcscpy_s(procname, _countof(procname), pProc);
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+	return CreateProcess(NULL, procname, NULL, NULL, 0, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+}
+
+int _tmain(int argc, LPTSTR argv[]) {
+
+	_tprintf(TEXT("Launching processes...\n"));
+	boot(TEXT("Server.exe"));
+	boot(TEXT("Gateway.exe"));
+	boot(TEXT("Client.exe"));
+	
+	return 0;
+}
--- Server/main.c
+++ Server/main.c
@@ -5,19 +5,6 @@
 #include <fcntl.h>
 #include "../DLL/dll.h"
 
-int boot_gateway() {
-
-	TCHAR	gateway[20];
-	PROCESS_INFORMATION pi;
-	STARTUPINFO si;
-
-	_tcscpy_s(gateway, _countof(gateway), TEXT("Gateway.exe"));
-	ZeroMemory(&si, sizeof(STARTUPINFO));
-	si.cb = sizeof(STARTUPINFO);
-
-	return CreateProcess(NULL, gateway, NULL, NULL, 0, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
-}
-
 int _tmain(int argc, LPTSTR argv[]) {
 
 	#ifdef UNICODE
@@ -27,18 +14,10 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 	_tprintf(TEXT("Paging file...\t\t\t[TBI]\n"));
 
-	_tprintf(TEXT("Launching gateway...\t\t"));
-	if(boot_gateway())
-		_tprintf(TEXT("[OK]\n"));
-	else {
-		_tprintf(TEXT("[ERROR]\n"));
-		return 1;
-	}
-
 	_tprintf(TEXT("Waiting for connection...\t[TBI]\n"));
 	_tprintf(TEXT("Reading value from dll: %d\n"),nDLL);
 
-
+	_gettchar();
 
 	return 0;
 }

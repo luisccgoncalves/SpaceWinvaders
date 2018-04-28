8d0f07d1ba3d6c604fc36ffc24ccf5bb35073a8c
simaohsferreira <simaohsferreira@users.noreply.github.com>
2018-04-29 00:57:10 +0200

    gateway  SM struct update

--- Gateway/gtw_main.c
+++ Gateway/gtw_main.c
@@ -25,48 +25,50 @@ int _tmain(int argc, LPTSTR argv[]) {
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
 
-	HANDLE			hSMem;		//Handle to shared memory
-	invader			*pSMem;		//Pointer to shared memory's first byte
-	LARGE_INTEGER	SMemSize;	//Stores the size of the mapped file
+	//HANDLE			hSMem;		//Handle to shared memory
+	//invader			*pSMem;		//Pointer to shared memory's first byte
+	//LARGE_INTEGER	SMemSize;	//Stores the size of the mapped file
+	//HANDLE			hNewMessage;
+	SMCtrl_Thread cThread;
 	HANDLE			hCanBootNow;
-	HANDLE			hNewMessage;
 	HANDLE			hStdout = GetStdHandle(STD_OUTPUT_HANDLE); //Handle to stdout to clear screen ##DELETE-ME after May 12th##
 
-	SMemSize.QuadPart = sizeof(invader); //This should be in structs.h or dll
+	cThread.SMemSize.QuadPart = sizeof(invader); //This should be in structs.h or dll
+	cThread.ThreadMustGoOn = 1;
 
 	//hCanBootNow = OpenEvent(EVENT_ALL_ACCESS, FALSE, (LPTSTR)TEXT("LetsBoot"));
 	hCanBootNow = CreateEvent(NULL, FALSE, FALSE, TEXT("LetsBoot"));
-	hNewMessage = CreateEvent(NULL, FALSE, FALSE, TEXT("NewMessage"));
+	cThread.hNewMessage = CreateEvent(NULL, FALSE, FALSE, TEXT("NewMessage"));
 
 
 	_tprintf(TEXT("Detecting if server is running.\n"));
 	WaitForSingleObject(hCanBootNow, 5000);
 
 	//Opens a mapped file by the server
-	hSMem = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, SMName);
-	if (hSMem == NULL) {
+	cThread.hSMem = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, SMName);
+	if (cThread.hSMem == NULL) {
 		_tprintf(TEXT("[Error] Opening file mapping (%d)\nIs the server running?\n"), GetLastError());
 		return -1;
 	}
 
 	//Creates a view of the desired part
-	pSMem = (invader *)MapViewOfFile(hSMem, FILE_MAP_ALL_ACCESS, 0, 0, SMemSize.QuadPart);
-	if (pSMem == NULL) {
+	cThread.pSMem = (invader *)MapViewOfFile(cThread.hSMem, FILE_MAP_ALL_ACCESS, 0, 0, cThread.SMemSize.QuadPart);
+	if (cThread.pSMem == NULL) {
 		_tprintf(TEXT("[Error] Mapping memory (%d)\nIs the server running?\n"), GetLastError());
 		return -1;
 	}
 
 	cls(hStdout);
 	hidecursor();
-	while (1) {//thread
-		WaitForSingleObject(hNewMessage, INFINITE);
+	while (cThread.ThreadMustGoOn) {//thread
+		WaitForSingleObject(cThread.hNewMessage, INFINITE);
 		cls(hStdout);
-		gotoxy(pSMem->x, pSMem->y);
+		gotoxy(cThread.pSMem->x, cThread.pSMem->y);
 		_tprintf(TEXT("W"));
 	}
 
-	UnmapViewOfFile(pSMem);
-	CloseHandle(hSMem);
+	UnmapViewOfFile(cThread.pSMem);
+	CloseHandle(cThread.hSMem);
 
 	return 0;
 }

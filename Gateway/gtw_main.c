d25929b142d64d814767e9922fefdfc06af5fddb
simaohsferreira <simaohsferreira@users.noreply.github.com>
2018-06-07 22:11:32 +0100

    moved all functions to comm, code cleaning

--- Gateway/gtw_main.c
+++ Gateway/gtw_main.c
@@ -1,144 +1,6 @@
 #include "comm.h"
 
 //#########################################################################################
-//############################   TEMP TEST   ##############################################
-//#########################################################################################
-
-//void addClient(HANDLE *c, HANDLE *newClient) {
-//	for (int i = 0; i < MAX_PLAYERS; i++) {
-//		if (c[i] == NULL) {
-//			c[i] = newClient;
-//			return;
-//		}
-//	}
-//}
-//
-//void removeClient(HANDLE *c, HANDLE *oldClient) {
-//	for (int i = 0; i < MAX_PLAYERS; i++) {
-//		if (c[i] == oldClient) {				
-//			c[i] = NULL;
-//			return;
-//		}
-//	}
-//}
-
-//int writeGameData(HANDLE hPipe, HANDLE *writeReady, PipeGameData *game) {
-//
-//	BOOL fSuccess = FALSE;
-//	DWORD cbWriten = 0;
-//
-//	OVERLAPPED overlWrite = { 0 };
-//	ResetEvent(writeReady);
-//	overlWrite.hEvent = writeReady;
-//
-//	fSuccess = WriteFile(
-//		hPipe,
-//		&game,
-//		sizeof(PipeGameData),
-//		&cbWriten,
-//		&overlWrite);
-//
-//	WaitForSingleObject(writeReady, INFINITE);
-//
-//	GetOverlappedResult(
-//		hPipe, 
-//		&overlWrite, 
-//		&cbWriten, 
-//		FALSE);
-//	if (cbWriten < sizeof(PipeGameData)) {
-//		_tprintf(TEXT("[Error] OVERLAPPED writeGameData (%d)\n"), GetLastError());
-//		return 1;
-//	}
-//
-//}
-
-//WIP
-int writePipeMsg(HANDLE hPipe, HANDLE writeReady, GameData msg) {
-	/*
-	This is for sending specific messages to a 
-	single pipe.
-	One exmaple may be player dead for the player
-
-	Probably we will not be able to make the pipe send two different messages
-	it's easier to include THIS gtw msg on writeGameData
-	*/
-
-		BOOL fSuccess = FALSE;
-		DWORD cbWriten = 0;
-
-		OVERLAPPED overlWrite = { 0 };
-
-		ResetEvent(writeReady);
-		overlWrite.hEvent = writeReady;
-
-		fSuccess = WriteFile(
-			hPipe,
-			&msg,
-			sizeof(msg),
-			&cbWriten,
-			&overlWrite);
-		if (!fSuccess) {
-			if (GetLastError() == ERROR_IO_INCOMPLETE)
-				return -1;
-		}
-
-		WaitForSingleObject(writeReady, INFINITE);
-
-		GetOverlappedResult(
-			hPipe,
-			&overlWrite,
-			&cbWriten,
-			FALSE);
-		if (cbWriten < sizeof(msg)) {
-			_tprintf(TEXT("[Error] OVERLAPPED writePipeMsgs (%d)\n"), GetLastError());
-			return 1;
-		}
-
-		return 0;
-}
-
-int readPipePacket(HANDLE hPipe, HANDLE readReady, Packet * pipePacket) { //probably the event needs to be a pointer
-
-	OVERLAPPED	OvrRd = { 0 };
-	DWORD		dwBytesRead = 0;
-	BOOL		bSuccess = FALSE;
-
-	//Packet		pipePacket;
-
-	OvrRd.hEvent = readReady;
-	ResetEvent(readReady);
-
-	bSuccess = ReadFile(
-		hPipe,
-		pipePacket,
-		sizeof(Packet),
-		&dwBytesRead,
-		&OvrRd);
-
-	WaitForSingleObject(readReady, INFINITE);
-
-	GetOverlappedResult(
-		hPipe,
-		&OvrRd,
-		&dwBytesRead,
-		FALSE);
-
-	if (dwBytesRead < sizeof(Packet)) {
-		if (GetLastError() == ERROR_BROKEN_PIPE) {
-			_tprintf(TEXT("[Error] Connection lost.\n"));
-			return -1;
-		}
-		else
-			_tprintf(TEXT("[Error] ReadFile failed. Error = %d \n"), GetLastError());
-	}
-
-	return 0;
-}
-
-
-
-
-//#########################################################################################
 /*
 To test move this to client
 */
@@ -167,13 +29,10 @@ int _tmain(int argc, LPTSTR argv[]) {
 	SMCtrl		cThread;
 	HANDLE		hCanBootNow;
 
-	//HANDLE		hSSendMessage;
-	//DWORD		tSendMessageID;
-
 	HANDLE		htCreatePipes;
 	DWORD		tCreatePipesID;
 
-	HANDLE		hStdout = GetStdHandle(STD_OUTPUT_HANDLE); //Handle to stdout to clear screen ##DELETE-ME after May 12th##
+	//HANDLE		hStdout = GetStdHandle(STD_OUTPUT_HANDLE); //Handle to stdout to clear screen ##DELETE-ME after May 12th##
 
 	SYSTEM_INFO	SysInfo;
 	DWORD		dwSysGran;
@@ -190,7 +49,7 @@ int _tmain(int argc, LPTSTR argv[]) {
 	hCanBootNow = OpenEvent(EVENT_ALL_ACCESS, FALSE, EVE_BOOT);
 	if (!hCanBootNow) {
 		hCanBootNow = CreateEvent(NULL, FALSE, FALSE, EVE_BOOT);
-		_tprintf(TEXT("Waiting for server to boot.\n"));
+		_tprintf(TEXT("[DEBUG] Waiting for server to boot.\n"));
 		WaitForSingleObject(hCanBootNow, INFINITE);
 	}
 
@@ -275,20 +134,8 @@ int _tmain(int argc, LPTSTR argv[]) {
 		_tprintf(TEXT("[Error] Creating thread CreatePipes (%d) at Gateway\n"), GetLastError());
 	}
 
-	//hSSendMessage = CreateThread(
-	//	NULL,								//Thread security attributes
-	//	0,									//Stack size
-	//	sendPacketServer,					//Thread function name
-	//	(LPVOID)&cThread,					//Thread parameter struct
-	//	0,									//Creation flags
-	//	&tSendMessageID);					//gets thread ID to close it afterwards
-	//if (hSSendMessage == NULL) {
-	//	_tprintf(TEXT("[Error] Creating thread SendMessage (%d) at Gateway\n"), GetLastError());
-	//}
-
 	//cThread.ThreadMustGoOn = 0;
 
-	//WaitForSingleObject(hSSendMessage, INFINITE);
 	WaitForSingleObject(htCreatePipes, INFINITE);
 
 	UnmapViewOfFile(cThread.pSMemGameData);		//Unmaps view of shared memory

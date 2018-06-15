989d266ca898363950844c43f83145fdd922b973
simaohsferreira <simaohsferreira@users.noreply.github.com>
2018-06-15 19:03:11 +0100

    trackbar players got better

--- Server_GUI/resource.h
+++ Server_GUI/resource.h
@@ -3,8 +3,25 @@
 // Used by Server_GUI.rc
 //
 #define IDR_MENU1                       101
+#define IDD_FORMVIEW                    101
 #define IDD_DIALOG1                     102
 #define IDC_BUTTON1                     1001
+#define IDC_SLIDER1                     1002
+#define IDC_PLAYERS                     1002
+#define IDC_SLIDER2                     1003
+#define IDC_SLIDER3                     1004
+#define IDC_SLIDER4                     1005
+#define IDC_SLIDER5                     1006
+#define IDC_SLIDER6                     1007
+#define IDPLAYERLABEL                   1007
+#define IDC_SLIDER7                     1008
+#define IDC_SLIDER8                     1009
+#define IDC_SLIDER9                     1010
+#define IDC_SLIDER10                    1011
+#define IDC_SLIDER11                    1012
+#define IDC_SLIDER12                    1013
+#define IDC_SLIDER13                    1014
+#define IDC_SLIDER14                    1015
 #define ID_SETTINGS_CONFIGUREGAME       40001
 #define ID_SETTINGS_ABOUTE              40002
 #define ID_SETTINGS_CLOSESERVER         40003
@@ -13,9 +30,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40004
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

2666073f2018a27173a479b03c037ef18db6fc69
luisccgoncalves <luisccgoncalves@gmail.com>
2018-06-16 23:53:26 +0100

    splitted client main and view logic

---
+++ ClientGUI/client_view_logic.h
@@ -0,0 +1,30 @@
+#ifndef CLIENT_VIEW_LOGIC_H
+#define CLIENT_VIEW_LOGIC_H
+
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Login(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Logout(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    HighScores(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    ConfigureKeys(HWND, UINT, WPARAM, LPARAM);
+
+#endif // CLIENT_VIEW_LOGIC_H
